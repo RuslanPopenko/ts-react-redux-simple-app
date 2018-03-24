f56af28ac534905efcf623be25ea0f76ecb42d5e
Ruslan Popenko <ruslanpopenko@ukr.net>
2018-03-24 21:30:07 +0200

    Refactor css.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './js/config/registerServiceWorker';
-import './styles/scss/index.css';
+import './styles/css/index.css';
 import { todoStore } from './js/stores/TodoStore';
 import { TodoApp } from './js/components/TodoApp';
 
