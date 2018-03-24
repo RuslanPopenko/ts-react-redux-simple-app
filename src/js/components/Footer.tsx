8c2498542206c15dad939a3f5e559065a23cd28d
Ruslan Popenko <ruslanpopenko@ukr.net>
2018-03-24 21:30:33 +0200

    Refactor css.

--- src/js/components/Footer.tsx
+++ src/js/components/Footer.tsx
@@ -6,8 +6,8 @@ export class Footer extends React.Component<{}, {}> {
 
     render(): React.ReactNode {
         return (
-            <fieldset className="filters">
-                <legend className="filters__title">Show:</legend>
+            <fieldset className="filter-wrapper">
+                <legend className="filters-wrapper__title">Show:</legend>
                 <StatusContainer status={Status.ALL}>All</StatusContainer>
                 <StatusContainer status={Status.ACTIVE}>Active</StatusContainer>
                 <StatusContainer status={Status.COMPLETED}>Completed</StatusContainer>
